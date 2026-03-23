b6a08e61021b61d936ab26812ae5f35312d65bcf
Saha-Ovi <ovisaha3210@gmail.com>
2026-03-23 17:37:59 +0600

    chart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import './App.css'
 import DaisyNav from './components/DaisyNav/DaisyNav'
 import NavBar from './components/NavBar/NavBar'
 import PricingOptions from './components/PricingOptions/PricingOptions';
+import Rechart from './components/Rechart/Rechart';
 
 const pricingPromise=fetch("PricingData.json").then(res=>res.json());
+const resultPromise=fetch("StudentResult.json").then(res=>res.json());
 
 function App() {
 
@@ -16,7 +19,21 @@ function App() {
       </header>
       <main>
 
-        <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
+       <Suspense fallback={
+        <div className='flex justify-center items-center'>
+          <span className='loading loading-ring loading-xl '></span>
+        </div>
+       }>
+         <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
+       </Suspense>
+
+       <Suspense fallback={
+        <div className='flex justify-center items-center'>
+          <span className='loading loading-ring loading-xl '></span>
+        </div>
+       }>
+        <Rechart resultPromise={resultPromise}></Rechart>
+       </Suspense>
       </main>
       <footer>
 
---
+++ src/components/Rechart/Rechart.jsx
@@ -0,0 +1,26 @@
+import React, { use } from 'react';
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+const Rechart = ({resultPromise}) => {
+    const resultPromiseData=use(resultPromise);
+    const resultData=resultPromiseData.students;
+    console.log(resultData);
+    return (
+        <div style={{width:"100%",maxWidth:600} } >
+            <LineChart width={1000} height={800} data={resultData}>
+                <XAxis dataKey={"name"}></XAxis>
+                <YAxis></YAxis>
+                <CartesianGrid></CartesianGrid>
+                <Tooltip></Tooltip>
+                 <Line dataKey={"math"} stroke='green'  ></Line>
+                 <Line dataKey={"physics"} stroke='brown'  ></Line>
+                 <Line dataKey="biology" stroke='blue'  ></Line>
+                 <Line dataKey={"chemistry"} stroke='red' ></Line>
+                 
+            </LineChart>
+           
+        </div>
+    );
+};
+
+export default Rechart;
